50745e5d6d5946866aaa3559573f17b08c148650
ken-9 <g031r066@s.iwate-pu.ac.jp>
2022-02-12 00:40:54 +0900

    [add]ホーム画面実装

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -15,23 +15,37 @@ class PostsTableSeeder extends Seeder
         $posts = 
         [
               [
+                // 集計期間外(現在時刻より12時間以上前)をちゃんと省くかのチェック用レコード 
                 'user_id'=>'1',
-                'player_id'=>'1',
-                'title'=>'test_title1',
-                'sentence'=>'test_sentence1',
+                'player_id'=>'1', // playersテーブルのid=1には確認用のデータが入っている 
+                'title'=>'check_term',
+                'sentence'=>'check_term',
+                'created_at'=>'2000-01-01 00:00:00'
               ],
               [
                 'user_id'=>'2',
-                'player_id'=>'2',
+                'player_id'=>'2', // メッシ
                 'title'=>'test_title2',
                 'sentence'=>'test_sentence2',
               ],
               [
                 'user_id'=>'3',
-                'player_id'=>'3',
+                'player_id'=>'3', // クリスティアーノ・ロナウド
                 'title'=>'test_title3',
                 'sentence'=>'test_sentence3',
-              ]
+              ],
+              [
+                'user_id'=>'4',
+                'player_id'=>'4', // 富安
+                'title'=>'test_title4',
+                'sentence'=>'test_sentence4',
+              ],
+              [
+                'user_id'=>'5',
+                'player_id'=>'2', // メッシ
+                'title'=>'test_title5',
+                'sentence'=>'test_sentence5',
+              ],
         ];
         
         // DBにデータ登録      
