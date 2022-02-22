c60fada5b5e9d1555ffaadd37d2bc08c03f08a4e
ken-9 <g031r066@s.iwate-pu.ac.jp>
2022-02-23 00:02:53 +0900

    [fix]herokuでのランキング表示

--- app/Player.php
+++ app/Player.php
@@ -12,37 +12,18 @@ class Player extends Model
     {
         // 直近一日で投稿数が多い選手のIDを取ってくる
         $date = new Carbon('-1 day');
-        
-        /*$ranks = Player::withCount(['posts'=> function (Builder $query) use ($date) {
-                       $query->whereDate('created_at','>',$date);  // where 現在時刻から1日以内
-                       }])
-                       ->having('posts_count','>','0')   // posts_count=0のものは省く(whereDateだけでは、posts_count=0のレコードとして取ってきてしまう)
-                       ->orderBy('posts_count', 'DESC')  // 投稿が多い選手順に並び変え
-                       ->limit(10)
-                       ->get();*/
-                       
-        /* $result = Player::withCount(['posts'=> function (Builder $query) use ($date) {
-                       $query->whereDate('created_at','>',$date);  // where 現在時刻から1日以内
-                       }])->get(); // ここまで上手くいった
-                       
-                       dump($result);
-                       
-        $ranks=$result->where($result->posts_count,'>','0')   // posts_count=0のものは省く(whereDateだけでは、posts_count=0のレコードとして取ってきてしまう)
-                       ->sortBy('posts_count')  // 投稿が多い選手順に並び変え
-                       ->limit(10)
-                       ->get();*/
                        
         $query=Player::withCount(['posts'=> function (Builder $query2) use ($date) {
                        $query2->whereDate('created_at','>',$date);  // where 現在時刻から1日以内
                        }]);
         
         $ranks=Player::fromSub($query, 'alias')
-            ->where('posts_count', '>', '0')
-            ->orderBy('posts_count', 'DESC')  // 投稿が多い選手順に並び変え
+            ->where('posts_count', '>', '0') // havingだとherokuで動かない
+            ->orderBy('posts_count', 'DESC') // 投稿が多い選手順に並び変え
             ->limit(10)
             ->get();              
                        
-                       return $ranks;
+        return $ranks;
     }
     
     public function getPlayerInfo() // 選手情報を取得
